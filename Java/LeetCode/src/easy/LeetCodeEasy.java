dfea1743158fe71efbe637d8d6a40bff41f311dd
unknown <jorra04@my.yorku.ca>
2021-06-23 13:54:02 -0400

    Added leetcode question.

--- Java/LeetCode/src/easy/LeetCodeEasy.java
+++ Java/LeetCode/src/easy/LeetCodeEasy.java
@@ -52,12 +52,11 @@ public class LeetCodeEasy {
 //		int k = 2;
 //		
 //		System.out.println(kLengthApart(nums, k));
-		
+
 //		int a = 1;
 //		int b  = 0;
 //		System.out.println(b=~b);
-		
-		
+
 //		
 //		int[][] indices = {
 //				{0,1},
@@ -68,95 +67,338 @@ public class LeetCodeEasy {
 //		
 //		
 //		System.out.println(oddCells(2, 3, indices));
-		String allowed = "ab";
+//		String allowed = "ab";
+//
+//		String[] words = { "ad", "bd", "aaab", "baa", "badab" };
+//
+//		System.out.println(countConsistentStrings(allowed, words));
+
+//		String word = "Leetcode";
+//		
+//		System.out.println(detectCapitalUse(word));
+
+//		int[] arr = {1,5,4,22};
+//		int l = 0;
+//		int h = arr.length -1;
+//		System.out.println(recursiveFindMax(arr, l, h));
+
+//		int[] arr1 = {1, 2, 11, 27};
+//		int[] arr2 = {4, 12, 19, 22};
+//		
+//		System.out.println(Arrays.toString(minAbsDiff(arr1, arr2)));
+
+//		System.out.println(isSubstring("eee", "eeeee"));
+
+//		int[][] mat = {
+//				{1,0,0},
+//				{0,0,1},
+//				{1,0,0}
+//		};
+//		System.out.println(numSpecial(mat));
+
+//		System.out.println(isSub("eeee" , "eeeee"));
+//		int[] arr = {3,71,3,71,71,71,71,42, 72};
+//		int high = arr.length -1;
+//		int low = 0;
+//		HashMap<Integer, Integer> ds = new HashMap();
+
+//		System.out.println(winningDay(arr, low, high, ds));
 		
-		String [] words = {"ad","bd","aaab","baa","badab"};
+		int[][] matrix = {{1,1,1},{1,0,1},{1,1,1}};
+		setZeroes(matrix);
 		
-		System.out.println(countConsistentStrings(allowed, words));
-		
-		
+		System.out.println(Arrays.toString(matrix));
 
 	}
-	
-	
+
+	public static void setZeroes(int[][] matrix) {
+
+		int[][] copyMat = new int[matrix.length][matrix[0].length];
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				copyMat[i][j] = matrix[i][j];
+			}
+		}
+
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				if (matrix[i][j] == 0) {
+					setRows(copyMat, i, j);
+					setCols(copyMat, i, j);
+				}
+			}
+		}
+
+		matrix = copyMat;
+	}
+
+	public static void setRows(int[][] matrix, int row, int col) {
+		for (int i = 0; i < matrix.length; i++) {
+			matrix[i][col] = 0;
+		}
+	}
+
+	public static void setCols(int[][] matrix, int row, int col) {
+		for (int i = 0; i < matrix[row].length; i++) {
+			matrix[row][i] = 0;
+		}
+	}
+
+	public static int winningDay(int[] arr, int low, int high, HashMap<Integer, Integer> ds) {
+
+		if (high == low) {
+			if (ds.containsKey(arr[high])) {
+				ds.put(arr[high], ds.get(arr[high]) + 1);
+			} else {
+				ds.put(arr[high], 1);
+			}
+
+			return arr[high];
+
+		}
+
+		int mid = (low + high) / 2;
+		int left = winningDay(arr, low, mid, ds);
+		int right = winningDay(arr, mid + 1, high, ds);
+
+		if (ds.get(left) > ds.get(right)) {
+			return left;
+		} else {
+			return right;
+		}
+
+	}
+
+	public static boolean isSub(String s1, String s2) {
+
+		if (s1.length() > s2.length()) {
+			return false;
+		}
+
+		for (int i = 1; i < s2.length(); i++) {
+			boolean isSub = true;
+			for (int j = 1; j < s1.length(); j++) {
+				if (s1.charAt(j) != s2.charAt(i + j - 1)) {
+					isSub = false;
+					break;
+				}
+			}
+
+			if (isSub) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public static int numSpecial(int[][] mat) {
+		int numSpecialPositions = 0;
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat[i].length; j++) {
+				if (mat[i][j] == 1 && (speacialRow(mat, j, i) && specialCol(mat, i, j))) {
+					numSpecialPositions++;
+				}
+			}
+		}
+
+		return numSpecialPositions;
+	}
+
+	public static boolean speacialRow(int[][] mat, int col, int row) {
+		for (int i = 0; i < mat.length; i++) {
+			if (mat[i][col] != 0 && i != row)
+				return false;
+		}
+
+		return true;
+	}
+
+	public static boolean specialCol(int[][] mat, int row, int col) {
+		for (int i = 0; i < mat[0].length; i++) {
+			if (mat[row][i] != 0 && i != col)
+				return false;
+		}
+
+		return true;
+	}
+
+	public static int recursiveFindMax(int[] arr, int l, int h) {
+		if (l == h) {
+			return arr[l];
+		}
+
+		int mid = (l + h) / 2;
+		int max1 = recursiveFindMax(arr, l, mid);
+		int max2 = recursiveFindMax(arr, mid + 1, h);
+
+		return Math.max(max1, max2);
+
+	}
+
+	public static boolean isSubstring(String s1, String s2) {
+		if (s1.length() > s2.length()) {
+			return false;
+		}
+		boolean isSub = true;
+		for (int i = 0; i < s2.length(); i++) {
+			isSub = true;
+			for (int j = 0; j < s1.length(); j++) {
+				if (s1.charAt(j) != s2.charAt(i + j)) {
+					isSub = false;
+					break;
+				}
+			}
+		}
+
+		return isSub;
+	}
+
+	public static int[] minAbsDiff(int[] arr1, int[] arr2) {
+		int headA = 0;
+		int headB = 0;
+		int minDiff = Integer.MAX_VALUE;
+		int[] smallestPair = { -1, -1 };
+
+		while (headA < arr1.length && headB < arr2.length) {
+			if (Math.abs(arr1[headA] - arr2[headB]) < minDiff) {
+				minDiff = Math.abs(arr1[headA] - arr2[headB]);
+				smallestPair = new int[] { headA, headB };
+			}
+
+			if (arr1[headA] < arr2[headB]) {
+				headA++;
+			} else {
+				headB++;
+			}
+		}
+		return smallestPair;
+	}
+
+//	public static int recursiveFindMax(int[] arr, int l, int h) {
+//		if(l >= h) {
+//			return arr[l];
+//		}
+//		
+//		
+//		int mid = (l+h)/2;
+//		int max1 = recursiveFindMax(arr, l, mid);
+//		int max2 = recursiveFindMax(arr, mid + 1, h);
+//		
+//		return Math.max(max1, max2);
+//	}
+
+	public static boolean detectCapitalUse(String word) {
+
+		char[] charArray = word.toCharArray();
+
+		return AllCaps(charArray) || noCaps(charArray) || firstLetterCaps(charArray);
+	}
+
+	public static boolean AllCaps(char[] charArray) {
+
+		for (char c : charArray) {
+			if (!(c >= 65 && c <= 90)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	public static boolean noCaps(char[] charArray) {
+
+		for (char c : charArray) {
+			if ((c >= 65 && c <= 90)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	public static boolean firstLetterCaps(char[] charArray) {
+		if (!(charArray[0] >= 65 && charArray[0] <= 90)) {
+			return false;
+		}
+
+		for (int i = 1; i < charArray.length; i++) {
+			if ((charArray[i] >= 65 && charArray[i] <= 90)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
 	public static int countConsistentStrings(String allowed, String[] words) {
-        int consistentStrings = 0;
-        int[] charCountsAllowed = new int[26];
-        
-        for(char c : allowed.toCharArray()){
-            charCountsAllowed[c -'a'] ++;
-        }
-        
-         // System.out.println(Arrays.toString(charCountsAllowed) +"\n\n");
-        
-        for(String word : words) {
-            int[] tempCharCountsAllowed = new int[26];
-        
-            for(char c : word.toCharArray()){
-                tempCharCountsAllowed[c -'a'] ++;
-            }
-            
-            // System.out.println(Arrays.toString(tempCharCountsAllowed));
-            boolean mismatch = false;
-            for(int i = 0; i < 26 && !mismatch; i ++) {
-                mismatch = !(tempCharCountsAllowed[i] >= charCountsAllowed[i]);
-            
-            }
-            if(!mismatch) {
-                consistentStrings++;
-            }
-            
-            
-        }
-        
-        
-        return consistentStrings;
-        
-        
-    }
-	
-	
-	 public static int oddCells(int m, int n, int[][] indices) {
-	        int[][] matrix = new int[m][n];
-	        
-	        for(int[] instructions : indices) {
-	            changeRowValues(matrix, instructions[0]);
-	            changeColValues(matrix, instructions[1]);
-	        }
-	        
-	        
-	        return checkOdds(matrix);
-	    }
-	    
-	    
-	    
-	    public static int checkOdds(int[][] matrix) {
-	        int oddCounter = 0;
-	        for(int[] row : matrix) {
-	            for(int i : row) {
-	                if(i % 2 != 0 ){
-	                    oddCounter ++;
-	                }
-	            }
-	        }
-	        
-	        
-	        return oddCounter;
-	    }
-	    
-	    
-	    
-	    public static void changeRowValues(int[][] matrix, int row) {
-	        for(int i = 0; i <matrix[row].length; i ++) {
-	        	matrix[row][i] ++;
-	        }
-	    }
-	    
-	    public static void changeColValues(int[][] matrix, int col) {
-	        for(int i = 0; i <matrix.length; i ++) {
-	        	matrix[i][col] ++;
-	        }
-	    }
+		int consistentStrings = 0;
+		int[] charCountsAllowed = new int[26];
+
+		for (char c : allowed.toCharArray()) {
+			charCountsAllowed[c - 'a']++;
+		}
+
+		// System.out.println(Arrays.toString(charCountsAllowed) +"\n\n");
+
+		for (String word : words) {
+			int[] tempCharCountsAllowed = new int[26];
+
+			for (char c : word.toCharArray()) {
+				tempCharCountsAllowed[c - 'a']++;
+			}
+
+			// System.out.println(Arrays.toString(tempCharCountsAllowed));
+			boolean mismatch = false;
+			for (int i = 0; i < 26 && !mismatch; i++) {
+				mismatch = !(tempCharCountsAllowed[i] >= charCountsAllowed[i]);
+
+			}
+			if (!mismatch) {
+				consistentStrings++;
+			}
+
+		}
+
+		return consistentStrings;
+
+	}
+
+	public static int oddCells(int m, int n, int[][] indices) {
+		int[][] matrix = new int[m][n];
+
+		for (int[] instructions : indices) {
+			changeRowValues(matrix, instructions[0]);
+			changeColValues(matrix, instructions[1]);
+		}
+
+		return checkOdds(matrix);
+	}
+
+	public static int checkOdds(int[][] matrix) {
+		int oddCounter = 0;
+		for (int[] row : matrix) {
+			for (int i : row) {
+				if (i % 2 != 0) {
+					oddCounter++;
+				}
+			}
+		}
+
+		return oddCounter;
+	}
+
+	public static void changeRowValues(int[][] matrix, int row) {
+		for (int i = 0; i < matrix[row].length; i++) {
+			matrix[row][i]++;
+		}
+	}
+
+	public static void changeColValues(int[][] matrix, int col) {
+		for (int i = 0; i < matrix.length; i++) {
+			matrix[i][col]++;
+		}
+	}
 
 	public static boolean kLengthApart(int[] nums, int k) {
 
@@ -172,7 +414,7 @@ public class LeetCodeEasy {
 		for (int i = 1; i < foundOnes.size(); i++) {
 			System.out.println(foundOnes.get(i).position - foundOnes.get(i - 1).position);
 
-			if (foundOnes.get(i).position - foundOnes.get(i - 1).position -1 < k) {
+			if (foundOnes.get(i).position - foundOnes.get(i - 1).position - 1 < k) {
 				return false;
 			}
 		}
